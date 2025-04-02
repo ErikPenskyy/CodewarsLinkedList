86c5929c421540f67acb68ccb073c4d6bda71733
p_erik <penskyi.pn@ucu.edu.ua>
2025-04-02 18:42:48 +0300

    Create alternating_split.py
    splits a linked list by odd and and even order number

---
+++ Linked Lists - Alternating Split/alternating_split.py
@@ -0,0 +1,25 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+def alternating_split(head):
+    if not head or not head.next:
+        raise ValueError("List must contain at least two nodes")
+    odd_head = head
+    even_head = head.next
+    odd = odd_head
+    even = even_head
+
+    while even and even.next:
+        odd.next = even.next
+        odd = odd.next
+        even.next = odd.next
+        even = even.next
+    odd.next = None
+    return Context(odd_head, even_head)
